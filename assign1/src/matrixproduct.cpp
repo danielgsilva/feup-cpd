f1294541bace5cbd2265772f166aac3cd57a1c8f
beatrizbernardoo <beatrizxalexandra@gmail.com>
2025-02-26 11:31:09 +0000

    matrixproduct

--- assign1/src/matrixproduct.cpp
+++ assign1/src/matrixproduct.cpp
@@ -75,15 +75,142 @@ void OnMult(int m_ar, int m_br)
 // add code here for line x line matriz multiplication
 void OnMultLine(int m_ar, int m_br)
 {
-    
-    
+	SYSTEMTIME Time1, Time2;
+
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+
+	pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for (i = 0; i < m_ar; i++)
+		for (j = 0; j < m_ar; j++)
+			pha[i * m_ar + j] = (double)1.0;
+
+	for (i = 0; i < m_br; i++)
+		for (j = 0; j < m_br; j++)
+			phb[i * m_br + j] = (double)(i + 1);
+
+	Time1 = clock();
+
+	for (i = 0; i < m_ar; i++)
+	{
+		for (k = 0; k < m_ar; k++)
+		{
+			temp = pha[i * m_ar + k];
+			for (j = 0; j < m_br; j++)
+			{
+				phc[i * m_br + j] += temp * phb[k * m_br + j];
+			}
+		}
+	}
+
+	Time2 = clock();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for (i = 0; i < 1; i++)
+	{
+		for (j = 0; j < min(10, m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+	
+
+	/*// display all elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for (i = 0; i < m_ar; i++)
+	{
+		for (j = 0; j < m_br; j++)
+			cout << phc[i * m_ar + j] << " ";
+		cout << endl;
+	}*/
+
+	free(pha);
+	free(phb);
+	free(phc);    
 }
 
 // add code here for block x block matriz multiplication
 void OnMultBlock(int m_ar, int m_br, int bkSize)
 {
-    
-    
+	SYSTEMTIME Time1, Time2;
+
+	char st[100];
+	double temp;
+	int i, j, k, ii, jj, kk;
+
+	double *pha, *phb, *phc;
+
+	pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for (i = 0; i < m_ar; i++)
+		for (j = 0; j < m_ar; j++)
+			pha[i * m_ar + j] = (double)1.0;
+
+	for (i = 0; i < m_br; i++)
+		for (j = 0; j < m_br; j++)
+			phb[i * m_br + j] = (double)(i + 1);
+
+	/*
+	double exemploA[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	double exemploB[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+	//double resultado[9] = {30, 24, 18, 84, 69, 54, 138, 114, 90};
+
+	for (i = 0; i < m_ar; i++)
+		for (j = 0; j < m_br; j++)
+		{
+			pha[i * m_ar + j] = exemploA[i * m_ar + j];
+			phb[i * m_br + j] = exemploB[i * m_br + j];
+		}
+	*/
+
+	Time1 = clock();
+
+	for (ii = 0; ii < m_ar; ii += bkSize)
+		for (jj = 0; jj < m_br; jj += bkSize)
+			for (kk = 0; kk < m_ar; kk += bkSize)
+				for (i = ii; i < min(ii + bkSize, m_ar); i++)
+					for (j = jj; j < min(jj + bkSize, m_br); j++)
+						for (k = kk; k < min(kk + bkSize, m_ar); k++)
+							phc[i * m_ar + j] += pha[i * m_ar + k] * phb[k * m_br + j];
+
+	Time2 = clock();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for (i = 0; i < 1; i++)
+	{
+		for (j = 0; j < min(10, m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+	
+
+	/*// display all elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for (i = 0; i < m_ar; i++)
+	{
+		for (j = 0; j < m_br; j++)
+			cout << phc[i * m_ar + j] << " ";
+		cout << endl;
+	}*/
+
+	free(pha);
+	free(phb);
+	free(phc);
 }
 
 
