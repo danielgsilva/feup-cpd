b631482bb63803cf95aca8372adc80257ee3bffb
Beatriz <up202206098@edu.fe.up.pt>
2025-03-13 12:09:04 +0000

    fixes on java code

--- assign1/src/matrixproduct.java
+++ assign1/src/matrixproduct.java
@@ -3,34 +3,36 @@ import java.util.Scanner;
 public class matrixproduct {
     
     public static void onMult(int size) {
-        double[][] pha = new double[size][size];
-        double[][] phb = new double[size][size];
-        double[][] phc = new double[size][size];
+        double[] pha = new double[size * size];
+        double[] phb = new double[size * size];
+        double[] phc = new double[size * size];
 
         for (int i = 0; i < size; i++)
             for (int j = 0; j < size; j++)
-                pha[i][j] = 1.0;
+                pha[i * size + j] = 1.0;
 
         for (int i = 0; i < size; i++)
             for (int j = 0; j < size; j++)
-                phb[i][j] = i + 1;
+                phb[i * size + j] = i + 1;
 
         long start = System.nanoTime();
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                phc[i][j] = 0;
+                double sum = 0;
                 for (int k = 0; k < size; k++) {
-                    phc[i][j] += pha[i][k] * phb[k][j];
+                    sum += pha[i * size + k] * phb[k * size + j];
                 }
+                phc[i * size + j] = sum;
             }
         }
+
         long end = System.nanoTime();
         System.out.printf("Time: %.3f seconds\n", (end - start) / 1e9);
 
         System.out.println("Result matrix:");
         for (int j = 0; j < Math.min(10, size); j++) {
-            System.out.print(phc[0][j] + " ");
+            System.out.print(phc[j] + " ");
         }
         System.out.println();
     }
